326b16fd81cbaf1a4ad0071cf084f937b0b9880d
GiacomoVisin <killerboss1150@gmail.com>
2026-05-20 15:35:53 +0200

    adding products page

---
+++ src/components/Products.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react"
+
+export default function Products() {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+            .then((res) => res.json())
+            .then((data) => setProducts(data))
+
+    }, [])
+    return (
+        <>
+            <h1>Prodotti</h1>
+
+            <div className="container">
+                <div className="row">
+                    {products.map((item) => (
+                        <div className="col col-lg-4 col-md-4 " key={item.id}>
+                            <div className="card p-3 mb-3">
+                                <img src={item.image} alt={item.description} style={{height: "250px"}} />
+                                <p> <strong>Title: </strong>{item.title} </p>
+                                <p> <strong>Price: </strong> {item.price} $ </p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+}
