484e9b0565d0932e4bd2d7ad79484e46e3395b65
GiacomoVisin <killerboss1150@gmail.com>
2026-05-21 18:26:19 +0200

    fixing page 404

--- src/components/Page404.jsx
+++ src/components/Page404.jsx
@@ -5,7 +5,7 @@ export default function Page404() {
     return (
         <div className="container">
 
-            <h1> ERROR, PAGE NOT FOUND</h1>
+            <h1> ERROR 404 , PAGE NOT FOUND</h1>
             <Link to="https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1" > Clicca qui per tornare al motore di ricerca</Link>
         </div>
     )
