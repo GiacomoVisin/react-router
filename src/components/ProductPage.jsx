83e6d43defa3f25003e98453a77ff1254e0f9355
GiacomoVisin <killerboss1150@gmail.com>
2026-05-21 16:49:52 +0200

    import useNavigate

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useParams,useNavigate } from "react-router-dom"
 export default function ProductPage() {
     const { id } = useParams()
     const [item, setCharacter] = useState([])
